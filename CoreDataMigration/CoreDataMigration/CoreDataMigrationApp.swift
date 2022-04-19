ecc6708e887805409092f25a691eec916fd8812a
haji44 <61269325+haji44@users.noreply.github.com>
2022-04-19 21:46:30 +0900

    InitialCommit

---
+++ CoreDataMigration/CoreDataMigration/CoreDataMigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMigrationApp.swift
+//  CoreDataMigration
+//
+//  Created by kitano hajime on 2022/04/19.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreDataMigration/CoreDataMigration/Model/PersistenceController.swift
@@ -0,0 +1,54 @@
+//
+//  PersistanceController.swift
+//  CoreDataMigration
+//
+//  Created by kitano hajime on 2022/04/19.
+//
+
+import Foundation
+import CoreData
+
+class PersistenceController {
+    static let shared = PersistenceController()
+
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+
+        let newPerson = PersonEntity(context: viewContext)
+        newPerson.name = "Taku"
+
+
+        let newClub = ClubEntity(context: viewContext)
+        newClub.name = "Swimming"
+        newClub.repesentitive = "Hajimel Phelps"
+
+
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("\(nsError.userInfo)")
+        }
+
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "CoreDataMigration")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url =
+            URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError(error.localizedDescription)
+            }
+        }
+
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
